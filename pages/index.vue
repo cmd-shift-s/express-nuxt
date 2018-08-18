6f8109e6dd3215bf1dc0735f007c4c3cc3d80f21
gongzza <gz.us.to@gmail.com>
2018-08-18 17:35:03 +0900

    Example echo page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,32 @@
 <template lang="html">
-  <h1>Hello World</h1>
+  <div>
+    <form @submit.prevent="onSubmit($event)">
+      <input type="text"><button type="submit">send</button>
+    </form>
+    echo: {{ msg }}
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+  data: () => ({
+    msg: ''
+  }),
+  methods: {
+    async onSubmit({target}) {
+      const inputEl = target.firstElementChild
+      const msg = inputEl.value
+      if (!msg) {
+        inputEl.focus()
+        return
+      }
+
+      const params = { msg }
+      const { data } = await this.$axios.get('echo', { params })
+      this.msg = data.msg
+    }
+  }
 }
 </script>
 
