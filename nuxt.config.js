8f81cc2f8a8654a898537bdbedcf4d832e3a1592
gongzza <gz.us.to@gmail.com>
2018-08-19 18:35:49 +0900

    Config(nuxt): add head tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,14 @@ require('dotenv').config()
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
+  head: {
+    title: 'Express & Nuxt',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Express & Nuxt' }
+    ]
+  },
   router: {
     base: process.env.ROUTER_BASE
   },
