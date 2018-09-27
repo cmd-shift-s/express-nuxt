2e612f3891afc76d0f470337a41511ef24065d75
gongzza <gz.us.to@gmail.com>
2018-09-27 18:33:58 +0900

    Update nuxt v2

--- app/components/pages/hello.tsx
+++ app/components/pages/hello.tsx
@@ -5,7 +5,7 @@ import { CreateElement, VNode } from 'vue'
 export default class extends Vue {
 
   render(h: CreateElement): VNode {
-    // return h('h1', 'Hello')
-    return <h1>Hello</h1>
+    return h('h1', 'Hello')
+    // return <h1>Hello</h1>
   }
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
-const path = require('path')
 
 require('dotenv').config()
+
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
